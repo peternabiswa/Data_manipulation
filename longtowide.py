d0c44d11f9324aeb765e0e4e2a48b387af68e63d
pnabiswa <peter.nabiswa@gmail.com>
2022-10-30 19:07:47 -0700

    Update longtowide.py

--- longtowide.py
+++ longtowide.py
@@ -1,12 +1,12 @@
 
 import pandas as pd
 
-df = pd.read_excel('C:\\Users\\v870603\\Documents\\Data\\Fitabase Data 4.12.16-5.12.16\\dailyCalories_merged.xlsx')
+df = pd.read_excel('C:\\Users\\Documents\\Data\\Fitabase Data 4.12.16-5.12.16\\dailyCalories_merged.xlsx')
 
 # Converting long data to wide data
 df_wide = df.pivot(index = 'Id', columns = 'ActivityDay', values = 'Calories')
 
-df_wide.to_excel(r'C:\\Users\\v870603\\Documents\\Data\\Fitabase Data 4.12.16-5.12.16\\dailyCalories_merged_wide.xlsx', sheet_name='dailyCalories_merged_wide', index = True)
+df_wide.to_excel(r'C:\\Users\\Documents\\Data\\Fitabase Data 4.12.16-5.12.16\\dailyCalories_merged_wide.xlsx', sheet_name='dailyCalories_merged_wide', index = True)
 
 print(df_wide)
 
